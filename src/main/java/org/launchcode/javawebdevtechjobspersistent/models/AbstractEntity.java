3140eee9d45dad1c9a1d9bb9990fd4e7b87fd61c
Kenan Kadic <ken.kadic@outlook.com>
2020-06-28 08:57:37 -0500

    name is NOT BLANK added

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
@@ -3,6 +3,8 @@ package org.launchcode.javawebdevtechjobspersistent.models;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 @MappedSuperclass
@@ -11,6 +13,8 @@ public abstract class AbstractEntity {
     @GeneratedValue
     private int id;
 
+    @NotBlank(message = "Name is required")
+    @Size(min = 3, max = 99, message = "Name must be between 3 and 99 characters long")
     private String name;
 
     public int getId() {
