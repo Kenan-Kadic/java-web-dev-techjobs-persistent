d94df32b2822946fca3b3eb5dff9b074ea976a60
Kenan Kadic <ken.kadic@outlook.com>
2020-06-28 12:11:39 -0500

    added zero argument constructors and description to skills class

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,13 +1,18 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+@Entity
 public class Employer extends AbstractEntity {
 
     @NotBlank(message = "Location is required")
     @Size(min = 1, max = 100, message = "Location must be between 1 and 100 characters long")
     private String location;
+
+// TODO maybe add super if it does not work
+    public Employer() {}
 
     public String getLocation() {
         return location;
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,20 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.validation.constraints.Size;
+
 public class Skill extends AbstractEntity {
 
-}
+    @Size(max = 500, message = "Description too long")
+    private String description;
+
+    public Skill() {
+    }
+
+    public String getDescription() {
+            return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+        }
+    }
