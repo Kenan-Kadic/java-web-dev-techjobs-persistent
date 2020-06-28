cc410da2ba580a45023e0bcebc20dcfcb8e1a7b1
Kenan Kadic <ken.kadic@outlook.com>
2020-06-28 12:13:08 -0500

    @Entity added to skills class

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,7 +1,9 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
 import javax.validation.constraints.Size;
 
+@Entity
 public class Skill extends AbstractEntity {
 
     @Size(max = 500, message = "Description too long")
