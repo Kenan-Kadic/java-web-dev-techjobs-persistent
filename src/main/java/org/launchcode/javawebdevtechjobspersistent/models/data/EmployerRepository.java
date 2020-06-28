e1ca5e12d383ded4a9bbb48c1fca78f090cd71b9
Kenan Kadic <ken.kadic@outlook.com>
2020-06-28 13:45:03 -0500

    added @AutoWired

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/data/EmployerRepository.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/data/EmployerRepository.java
@@ -4,6 +4,7 @@ import org.launchcode.javawebdevtechjobspersistent.models.Job;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+// TODO Check if phrase @Transitional is needed
 @Repository
 public interface EmployerRepository extends CrudRepository<Job, Integer> {
 }
