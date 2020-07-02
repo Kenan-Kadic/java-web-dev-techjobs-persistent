c07a1a32c185705ab8c97172b8e98deecc395406
Kenan Kadic <ken.kadic@outlook.com>
2020-07-02 18:09:51 -0500

    Part 3 Arraylist added

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -3,6 +3,8 @@ package org.launchcode.javawebdevtechjobspersistent.models;
 import javax.persistence.Entity;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -11,7 +13,8 @@ public class Employer extends AbstractEntity {
     @Size(min = 1, max = 100, message = "Location must be between 1 and 100 characters long")
     private String location;
 
-// TODO maybe add super if it does not work
+    private final List<Job> jobs = new ArrayList<>();
+
     public Employer() {}
 
     public String getLocation() {
